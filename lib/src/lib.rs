e3135561576f242b010d271a952df7db8c59f635
Till Schneidereit <till@tillschneidereit.net>
2025-01-14 16:01:16 +0100

    Move detection into its own crate and reuse it in `lib` and `directly-rust`

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -5,8 +5,6 @@ use bindings::exports::component::face_detection_lib::face_detection::{
 #[allow(warnings)]
 mod bindings;
 
-mod detection;
-
 struct Component;
 
 impl Guest for Component {
@@ -14,11 +12,26 @@ impl Guest for Component {
         img: Image,
         confidence_treshold: f32,
     ) -> Result<Vec<DetectionResult>, DetectionError> {
-        Ok(detection::detect(img, confidence_treshold)?)
+
+        let detections = detection::detect(img, confidence_treshold)?;
+        let detections = detections.into_iter().map(|d| d.into()).collect();
+        Ok(detections)
     }
 }
 
 bindings::export!(Component with_types_in bindings);
+
+impl From<detection::DetectionResult> for DetectionResult {
+    fn from(value: detection::DetectionResult) -> Self {
+        DetectionResult {
+            x1: value.x1,
+            y1: value.y1,
+            x2: value.x2,
+            y2: value.y2,
+            confidence: value.confidence,
+        }
+    }
+}
 
 impl From<anyhow::Error> for DetectionError {
     fn from(e: anyhow::Error) -> Self {
